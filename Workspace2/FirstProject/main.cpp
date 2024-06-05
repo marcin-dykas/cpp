fed44bc9e9d446f54eb07769d44cd4ecb067aab8
mdykas <mdykas@gorillagroup.com>
2024-06-05 23:02:03 +0200

    FirstProject

---
+++ Workspace2/FirstProject/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main() {
+    
+    int favoriteNumber;
+    
+    std::cout << "Eneter some number" << std::endl;
+    
+    std::cin >> favoriteNumber;
+    
+    std::cout << "nice " << favoriteNumber << std::endl;
+    
+    return 0;
+    
+}
